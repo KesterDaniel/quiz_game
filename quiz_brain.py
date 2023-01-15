e3c1ea8298ccc433108d11685f7a4efd7dfaccd8
KesterDaniel <kesterdan17@gmail.com>
2023-01-15 18:20:38 +0100

    check if questions still exists to repeat

--- quiz_brain.py
+++ quiz_brain.py
@@ -6,3 +6,8 @@ class QuizBrain:
     def next_question(self):
         current_question = self.question_list[self.question_number].text
         user_answer = input(f"Q.{self.question_number + 1}: {current_question} (True/False)?: ")
+        self.question_number += 1
+
+    def still_has_questions(self):
+        return self.question_number <= len(self.question_list) - 1
+
