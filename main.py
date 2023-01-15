d749fc2e826f5681ee8113a9dea8a16c13e40508
KesterDaniel <kesterdan17@gmail.com>
2023-01-15 17:23:32 +0100

    creating the list of question objects from the data

---
+++ main.py
@@ -0,0 +1,11 @@
+from data import question_data
+from question_model import Question
+
+
+question_bank = []
+
+for question in question_data:
+    question_bank.append(Question(question["text"], question["answer"]))
+
+print(question_bank[0].text)
+
