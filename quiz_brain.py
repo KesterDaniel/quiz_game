c3e17536dfa8aa8cf1c9c37909e47803df7bb5c0
KesterDaniel <kesterdan17@gmail.com>
2023-01-15 17:52:03 +0100

    created the next_question method

--- quiz_brain.py
+++ quiz_brain.py
@@ -3,4 +3,6 @@ class QuizBrain:
         self.question_number = 0
         self.question_list = question_list
 
-   
+    def next_question(self):
+        current_question = self.question_list[self.question_number].text
+        user_answer = input(f"Q.{self.question_number}: {current_question} (True/False)?: ")
