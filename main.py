a6c537dce2ee876d58f3c576558126c0ad9857ce
KesterDaniel <kesterdan17@gmail.com>
2023-01-15 18:01:07 +0100

    imported quizbrain into main.py to implement

--- main.py
+++ main.py
@@ -1,11 +1,12 @@
 from data import question_data
 from question_model import Question
-
+from quiz_brain import QuizBrain
 
 question_bank = []
 
 for question in question_data:
     question_bank.append(Question(question["text"], question["answer"]))
 
-print(question_bank[0].text)
+quiz = QuizBrain(question_bank)
+quiz.next_question()
 
--- quiz_brain.py
+++ quiz_brain.py
@@ -5,4 +5,4 @@ class QuizBrain:
 
     def next_question(self):
         current_question = self.question_list[self.question_number].text
-        user_answer = input(f"Q.{self.question_number}: {current_question} (True/False)?: ")
+        user_answer = input(f"Q.{self.question_number + 1}: {current_question} (True/False)?: ")
